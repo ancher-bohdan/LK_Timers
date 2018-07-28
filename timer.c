34e6295600366614c7573e2759e5433e0a6972e1
Bohdan Chupys <chupys.bohdan@gmail.com>
2018-07-28 16:11:26 +0300

    Adding code for hr_timer

--- timer.c
+++ timer.c
@@ -1,43 +1,48 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/timer.h>
+#include <linux/ktime.h>
+#include <linux/hrtimer.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Bohdan Chupys");
 MODULE_DESCRIPTION("Timer");
 MODULE_VERSION("0.22");
 
-static struct timer_list timer;
-static long period = 1000;
+unsigned long timer_interval_ns = 500000000;
+static struct hrtimer timer;
 
-void timer_fn(unsigned long data)
+enum hrtimer_restart timer_fn(struct hrtimer *timer_for_restart)
 {
   printk("Timer tick\n");
+  return HRTIMER_RESTART;
 }
 
-static int __init hello_init(void){
+static int __init timer_init(void){
 
-  int ret;
+  ktime_t ktime;
 
-  printk("[%s]: Timer module load\n", __func__);
-  setup_timer( &timer, timer_fn, 0 );
+  ktime = ktime_set(0, timer_interval_ns);
 
-  ret = mod_timer(&timer, jiffies + msecs_to_jiffies(period));
-  if(ret) printk("Error in mod_timer\n");
+  hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+  timer.function = &timer_fn;
+
+  hrtimer_start(&timer, ktime, HRTIMER_MODE_REL);
+
+  printk(KERN_ERR "Ready to start hrtimer");
+  printk("Timer start\n");
 
   return 0;
 }
 
-static void __exit hello_exit(void){
-  int ret;
+static void __exit timer_exit(void){
+  int cancelled = hrtimer_cancel(&timer);
 
-  ret = del_timer(&timer);
-
-  if(ret) printk("Timer still in use...\n");
-
-  printk("Timer module uuninstalling");
+  if(cancelled)
+    printk(KERN_ERR "Timer still running\n");
+  else
+    printk(KERN_ERR "Timer cancelled\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(timer_init);
+module_exit(timer_exit);
